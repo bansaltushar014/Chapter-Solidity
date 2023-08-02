7237d29bd691ccd9fdfe32f03808c299225803cf
Tushar Bansal <bansaltushar014@gmail.com>
2023-08-02 15:34:20 +0530

    TransaparentProxy!

---
+++ Proxies /TransparentProxy/OpenZepplinCall/MyProxy.sol
@@ -0,0 +1,13 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract MyProxy is TransparentUpgradeableProxy {
+    constructor(
+        address _logic,
+        address admin_,
+        bytes memory _data
+    ) payable TransparentUpgradeableProxy(_logic, admin_, _data) {}
+}
---
+++ Proxies /TransparentProxy/OpenZepplinCall/MyProxyAdmin.sol
@@ -0,0 +1,7 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+
+contract MyProxyAdmin is ProxyAdmin {}
