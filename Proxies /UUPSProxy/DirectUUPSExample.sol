eddbe835426f86ec367cb8f2cb56cec38778fa9b
Tushar Bansal <bansaltushar014@gmail.com>
2023-08-03 15:10:41 +0530

    UUPS Proxy!

---
+++ Proxies /UUPSProxy/DirectUUPSExample.sol
@@ -0,0 +1,96 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.1;
+
+contract Proxy {
+    // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
+    constructor(bytes memory constructData, address contractLogic) {
+        // save the code address
+        assembly { // solium-disable-line
+            sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, contractLogic)
+        }
+        (bool success, bytes memory result ) = contractLogic.delegatecall(constructData); // solium-disable-line
+        require(success, "Construction failed");
+    }
+
+    fallback() external payable {
+        assembly {
+            let target := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), target, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    function readValueUsingSload() public view returns (address) {
+        address value;
+        assembly {
+            value := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7) // Read using sload
+        }
+        return value;
+    }
+}
+
+contract Proxiable {
+    // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
+
+    function updateCodeAddress(address newAddress) internal {
+        require(
+            bytes32(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7) == Proxiable(newAddress).proxiableUUID(),
+            "Not compatible"
+        );
+        assembly { // solium-disable-line
+            sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, newAddress)
+        }
+    }
+
+    function proxiableUUID() public pure returns (bytes32) {
+        return 0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7;
+    }
+
+    function readValueUsingSload() public view returns (address) {
+        address value;
+        assembly {
+            value := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7) // Read using sload
+        }
+        return value;
+    }
+} 
+
+contract MyContract {
+
+    address public owner;
+    uint public myUint;
+
+    function constructor1() public {
+        require(owner == address(0), "Already initalized");
+        owner = msg.sender;
+    }
+
+    function increment() public {
+        myUint++;
+    }
+
+    // function decrement() public {
+    //     myUint--;
+    // }
+}
+
+contract MyFinalContract is MyContract, Proxiable {
+
+    function updateCode(address newCode) onlyOwner public {
+        updateCodeAddress(newCode);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner is allowed to perform this action");
+        _;
+    }
+}
---
+++ Proxies /UUPSProxy/Readme.md
@@ -0,0 +1,31 @@
+# Project Title
+
+Simple overview of UUPSProxy
+
+## Description
+
+This sample code represents the use of openzepplin proxy - "import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol"" & direct example. 
+
+## Getting Started
+
+### Dependencies
+
+* Remix
+
+
+### Installing
+
+
+### Executing program
+
+* Copy the code
+* Paste on the Remix
+
+## Help
+
+Any advise for new features to add into it. Open source contibutions are accepted. 
+
+
+## More Info 
+
+To know more about the code, visit this page ``
