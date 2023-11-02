d40a5482dd5ab9084394f128f785d403fab06362
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-11-02 17:05:39 +0000

    ERC6492

---
+++ ERC6492/Readme.sol
@@ -0,0 +1,28 @@
+# Project Title
+
+Simple overview of signature Validation for Predeploy Contracts
+
+
+## Getting Started
+
+### Dependencies
+
+* Remix
+
+
+### Installing
+
+
+### Executing program
+
+* Copy the code
+* Paste on the Remix
+
+## Help
+
+Any advise for new features to add into it. Open source contibutions are accepted. 
+
+
+## More Info 
+
+To know more about the code, visit this page ``
